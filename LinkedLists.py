999ffb1ab9a444fa41fbb530c557f9ea23913d83
Physix <159613996+1physix@users.noreply.github.com>
2025-05-13 10:48:06 +0000

    yuh

--- LinkedLists.py
+++ LinkedLists.py
@@ -9,18 +9,20 @@ class SingleNode: #initiate Node class
     def __str__(self):
         return str(self.val)
 
-head1 = SingleNode(1) #setting up the head node and following nodes.
+#setting up the head node and following nodes.
 A1 = SingleNode(3)
+head1 = SingleNode(1)
 B1 = SingleNode(5)
 C1 = SingleNode(7)
 
-head1.next = A1 # how to actually link the lists
+# how to actually link the lists
+head1.next = A1
 A1.next = B1
 B1.next = C1
 
 def display1(head): # making a way to display the list in a nice fashion.
     elements = []
-    """How to traverse a singly linked list"""
+    """How to traverse a singly linked list - O(n)"""
     curr = head
     while curr:
         elements.append(str(curr.val))
@@ -29,6 +31,26 @@ def display1(head): # making a way to display the list in a nice fashion.
     print(" -> ".join(elements))
 
 #display1(head)
+
+#Search for a node value - O(n)
+def search(head, val):
+    curr = head
+    while curr:
+        if curr.val == val:
+            return True
+        curr = curr.next
+
+search(head1, 3)
+
+
+
+
+
+
+
+
+
+
 
 
 #Doubly-Linked Lists
@@ -68,4 +90,4 @@ def display2(head): #same display function, traversal is the same
         curr = curr.next
     print(" <-> ".join(elements))
 
-display2(head2)
+#display2(head2)
